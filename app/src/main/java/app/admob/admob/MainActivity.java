c941af7a63308198eeca3b4c534002acc2291c11
cristian_dev <cristian_dev@pmovil.com.ar>
2015-10-02 10:29:04 -0300

    Initial commit

---
+++ app/src/main/java/app/admob/admob/MainActivity.java
@@ -0,0 +1,112 @@
+package screen.com.multiplesizes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+import screen.com.multiplesizes.R;
+
+public class MainActivity extends AppCompatActivity {
+    // Remove the below line after defining your own ad unit ID.
+    private static final String TOAST_TEXT = "Test ads are being shown. "
+            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+
+    private static final int START_LEVEL = 1;
+    private int mLevel;
+    private Button mNextLevelButton;
+    private InterstitialAd mInterstitialAd;
+    private TextView mLevelTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create the next level button, which tries to show an interstitial when clicked.
+        mNextLevelButton = ((Button) findViewById(R.id.next_level_button));
+        mNextLevelButton.setEnabled(false);
+        mNextLevelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInterstitial();
+            }
+        });
+
+        // Create the text view to show the level number.
+        mLevelTextView = (TextView) findViewById(R.id.level);
+        mLevel = START_LEVEL;
+
+        // Create the InterstitialAd and set the adUnitId (defined in values/strings.xml).
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
+
+        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
+        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
+    }
+
+
+    private InterstitialAd newInterstitialAd() {
+        InterstitialAd interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                mNextLevelButton.setEnabled(true);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                mNextLevelButton.setEnabled(true);
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Proceed to the next level.
+                goToNextLevel();
+            }
+        });
+        return interstitialAd;
+    }
+
+    private void showInterstitial() {
+        // Show the ad if it's ready. Otherwise toast and reload the ad.
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+            goToNextLevel();
+        }
+    }
+
+    private void loadInterstitial() {
+        // Disable the next level button and load the ad.
+        mNextLevelButton.setEnabled(true);
+        AdRequest request = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template")
+                /*.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators*/
+                .addTestDevice("D9B26731969B87BBCF8213A11376DCBF")  // My Nexus test phone
+                .build();
+        mInterstitialAd.loadAd(request);
+
+        /*AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        mInterstitialAd.loadAd(adRequest);*/
+
+        /*AdRequest.Builder.addTestDevice("D9B26731969B87BBCF8213A11376DCBF");*/
+
+    }
+
+    private void goToNextLevel() {
+        // Show the next level and reload the ad to prepare for the level after.
+        mLevelTextView.setText("Generando $0." + (++mLevel));
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
+    }
+}
